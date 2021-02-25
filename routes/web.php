c8dfb026945b7accd97d3543d854032b76806532
Álesson de Jesus <alesson.jesus@synapsebrasil.com.br>
2021-02-24 21:13:05 -0500

    feat: stisla

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+   return view('app.dashboard');
+});
+
+Route::get('/table', function () {
+    return view('app.table');
+});
